2deb6fea8211b9ee49de45b20d1b8622ea16f8b9
TaysWeb <snelltararobin@gmail.com>
2023-06-01 16:33:11 +0200

    index done

---
+++ assets/code.js
@@ -0,0 +1,24 @@
+function disPerson() {
+
+    //variables
+let name = document.getElementById("name") ;
+let Surname = document.getElementById("surname") ;
+let Age = document.getElementById("Age") ;
+let outcome =  document.getElementById("outcome");
+
+let people = {
+    FirstName : "Tara" ,
+    Surname : "Snell" ,
+    Age : 19 
+}
+
+document.getElementById("addP") = array.push(); 
+
+
+
+
+
+
+
+
+}
